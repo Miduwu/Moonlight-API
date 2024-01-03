8c03663bd39dd69034920f045b2124a72ce71f40
Mid <83442808+Miduwu@users.noreply.github.com>
2024-01-03 16:07:22 -0600

    Added serverless function

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,8 @@ export { Endpoint } from "./classes/Endpoint";
 export { Context } from "./classes/Context";
 export * from "./classes/Frame";
 export { server, aux }
+
+export default async (req: any, res: any) => {
+    await server.ready();
+    server.server.emit('request', req, res);
+  }
