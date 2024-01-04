6aaf5e7a4aaf72ac97842a30a2b01d51b08f9b90
Mid <83442808+Miduwu@users.noreply.github.com>
2024-01-03 18:09:01 -0600

    removed vercel tihngs

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,3 @@ export { Endpoint } from "./classes/Endpoint";
 export { Context } from "./classes/Context";
 export * from "./classes/Frame";
 export { server, aux }
-
-export default async (req: any, res: any) => {
-    await server.ready();
-    server.server.emit('request', req, res);
-  }
